7b7e5a82aaca78a4b70e1f583e08d927da307ecb
jim price <shadeops@gmail.com>
2022-05-25 17:37:53 -0700

    37 test

--- build.zig
+++ build.zig
@@ -1,5 +1,28 @@
 const std = @import("std");
-const raylib = @import("ext/raylib/src/build.zig");
+//const raylib = @import("ext/raylib/src/build.zig");
+
+fn buildRaylib(b: *std.build.Builder) *std.build.RunStep {
+    const cmake = b.addSystemCommand(&[_][]const u8{
+        "cmake",
+        "-B",
+        "ext/raylib/build",
+        "-S",
+        "ext/raylib",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DOpenGL_GL_PREFERENCE=GLVND",
+        "-DBUILD_EXAMPLES=OFF",
+    });
+    const cmake_build = b.addSystemCommand(&[_][]const u8{
+        "cmake",
+        "--build",
+        "ext/raylib/build",
+        "--",
+        "-j",
+        "16",
+    });
+    cmake_build.step.dependOn(&cmake.step);
+    return cmake_build;
+}
 
 fn buildCurl(b: *std.build.Builder) *std.build.RunStep {
     const cmake = b.addSystemCommand(&[_][]const u8{
@@ -35,14 +58,15 @@ pub fn build(b: *std.build.Builder) void {
 
     const build_ext = b.step("build-ext", "Build External Dependencies");
     build_ext.dependOn(&buildCurl(b).step);
+    build_ext.dependOn(&buildRaylib(b).step);
 
-    const raylib_step = raylib.addRaylib(b, target);
-    raylib_step.setOutputDir("ext/raylib/src");
+//    const raylib_step = raylib.addRaylib(b, target);
+//    raylib_step.setOutputDir("ext/raylib/src");
 
     const exe = b.addExecutable("sandfarm", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    exe.step.dependOn(&raylib_step.step);
+    //exe.step.dependOn(&raylib_step.step);
     exe.addIncludeDir("ext/raylib/src");
     exe.addObjectFile("ext/raylib/src/libraylib.a");
     exe.addIncludeDir("ext/curl/include");
