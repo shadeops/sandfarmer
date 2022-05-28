1800a3f887dd581837c1abcbccb621a9d8636fa2
jim price <shadeops@gmail.com>
2022-05-28 14:17:45 -0700

    Added more end points to the mock server.

--- utils/mock_server.py
+++ utils/mock_server.py
@@ -1,38 +1,95 @@
-import random
+import os
 import time
+import random
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
-@app.route("/Tractor/monitor", methods = ['GET', 'POST'])
-def hello_world():
-    query = request.args.get('q')
+hosts = [ "blade%i"%i for i in range(10) ]
+users = ["cat", "dog", "cow", "pig", "crow", "fish", "fly", os.environ.get("USER","")]
+division = ["good", "evil", "neutral"]
+dept = ["good", "evil", "neutral"]
+sub = ["chaotic", "lawful", "neutral"]
+unit = ["mages", "warriors", "rogues"]
+#division
+#dept.sub.unit
+
+min_tasks = 1
+max_tasks = 100
+
+@app.route("/Tractor/monitor", methods = ["GET", "POST"])
+def get_tasks():
+    query = request.form.get("q")
     if query == "login":
         return {"tsid" : "0123456789012345678901234567890123456"}
 
-    query = request.form.get('q')
-    if query == "login":
-        return {"tsid" : "0123456789012345678901234567890123456"}
+    min_arg = int(request.args.get("min", 1))
+    max_arg = int(request.args.get("max", 100))
 
     ret = {"mbox" : []}
-    for i in range(random.randint(10,1000)):
+    for i in range(random.randint(min_arg,max_arg)):
         cmd = [
             "c",
             random.randint(1,100), # jid
             random.randint(0,100), # tid
             random.randint(0,5), # cid
-            random.choice("ABDE"), # status
+            random.choices("ABDE", weights=[1, 0.05, 1, 0.2], k=1 )[0], # status
             1, # has log
-            "localhost", # hostname
+            random.choice(hosts), # hostname
             "9005", # port
             random.randint(0,100), # numtasks
             random.randint(0,100), # numactive
             random.randint(0,100), # numdone
             random.randint(0,100), # numerror
-            "jmp", # owner
+            random.choice(users), # owner
             time.time(), # unix time
         ]
         ret["mbox"].append(cmd)
+    return ret
+
+@app.route("/Tractor/config", methods = ["GET"])
+def get_blades():
+    query_arg = request.args.get("q")
+    file_arg = request.args.get("file")
+    if query_arg == "get" and file_arg == "blade.config":
+        ret = {"BladeProfiles" : []}
+        profile = {
+            "ProfileName" : "profile_1",
+            "Hosts" : {
+                "Name" : hosts[0:4]
+            }
+        }
+        ret["BladeProfiles"].append(profile)
+        profile = {
+            "ProfileName" : "profile_2",
+            "Hosts" : {
+                "Name" : hosts[4:9]
+            }
+        }
+        ret["BladeProfiles"].append(profile)
+        profile = {
+            "ProfileName" : "profile_3",
+            "Hosts" : {
+                "Name" : hosts[9:]
+            }
+        }
+        ret["BladeProfiles"].append(profile)
+        return ret
+
+    return "wut?"
+
+@app.route("/whoswho", methods = ["GET"])
+def whoswho():
+    ret = {}
+    for i,user in enumerate(users):
+        entry = {
+            "login" : user,
+            "division" : random.choice(division),
+            "dept" : random.choice(dept),
+            "sub" : random.choice(sub),
+            "unit" : random.choice(unit),
+        }
+        ret[str(i)] = entry
     return ret
 
