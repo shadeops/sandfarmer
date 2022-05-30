d239c519cb7ed5d902091b461b53530954db3c08
jim price <shadeops@gmail.com>
2022-05-29 18:49:53 -0700

    Move url requests into its own file

--- src/tractor.zig
+++ src/tractor.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
-
-const curl = @cImport(
-    @cInclude("curl/curl.h"),
-);
+const url = @import("url.zig");
 
 const MessageCounts = @import("mbox.zig").MessageCounts;
 
@@ -46,7 +43,7 @@ pub const Context = struct {
         //std.debug.print("Logging into Tractor\n", .{});
         var buf: [64:0]u8 = undefined;
         var post = try std.fmt.bufPrintZ(buf[0..], "q=login&user={s}", .{std.os.getenv("USER")});
-        var response = try postTractor(self.allocator, post, self.url);
+        var response = try url.request(self.allocator, post, self.url);
         defer response.deinit();
 
         var p = std.json.Parser.init(self.allocator, false);
@@ -63,75 +60,12 @@ pub const Context = struct {
     pub fn queryTractor(self: *Self) !?MessageCounts {
         var buf: [128:0]u8 = undefined;
         var post = try std.fmt.bufPrintZ(buf[0..], "q=subscribe&jids=0&tsid={s}", .{self.tsid.?});
-        var response = try postTractor(self.allocator, post, self.url);
+        var response = try url.request(self.allocator, post, self.url);
         defer response.deinit();
         //std.debug.print("\n\n{s}\n\n", .{response.items});
         return parseResponse(self.allocator, response);
     }
 };
-
-// From https://ziglang.org/learn/samples/#using-curl-from-zig
-fn writeToArrayListCallback(
-    data: *anyopaque,
-    size: c_uint,
-    nmemb: c_uint,
-    user_data: *anyopaque,
-) callconv(.C) c_uint {
-    var buffer = @intToPtr(*std.ArrayList(u8), @ptrToInt(user_data));
-    var typed_data = @intToPtr([*]u8, @ptrToInt(data));
-    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
-    return nmemb * size;
-}
-
-fn postTractor(
-    allocator: std.mem.Allocator,
-    post: []const u8,
-    url: []const u8,
-) !std.ArrayList(u8) {
-
-    // global curl init, or fail
-    if (curl.curl_global_init(curl.CURL_GLOBAL_ALL) != curl.CURLE_OK)
-        return error.CURLGlobalInitFailed;
-    defer curl.curl_global_cleanup();
-
-    // curl easy handle init, or fail
-    const handle = curl.curl_easy_init() orelse return error.CURLHandleInitFailed;
-    defer curl.curl_easy_cleanup(handle);
-
-    var response_buffer = std.ArrayList(u8).init(allocator);
-
-    // superfluous when using an arena allocator, but
-    // important if the allocator implementation changes
-    errdefer response_buffer.deinit();
-
-    // setup curl options
-    if (curl.curl_easy_setopt(handle, curl.CURLOPT_URL, url.ptr) != curl.CURLE_OK)
-        return error.CouldNotSetURL;
-
-    if (curl.curl_easy_setopt(handle, curl.CURLOPT_POSTFIELDS, post.ptr) != curl.CURLE_OK)
-        return error.CouldNotSetPost;
-
-    // set write function callbacks
-    if (curl.curl_easy_setopt(
-        handle,
-        curl.CURLOPT_WRITEFUNCTION,
-        writeToArrayListCallback,
-    ) != curl.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-
-    if (curl.curl_easy_setopt(
-        handle,
-        curl.CURLOPT_WRITEDATA,
-        &response_buffer,
-    ) != curl.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-
-    // perform
-    if (curl.curl_easy_perform(handle) != curl.CURLE_OK)
-        return error.FailedToPerformRequest;
-
-    return response_buffer;
-}
 
 fn parseResponse(allocator: std.mem.Allocator, response: std.ArrayList(u8)) !?MessageCounts {
     var p = std.json.Parser.init(allocator, false);
---
+++ src/url.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+
+const curl = @cImport(
+    @cInclude("curl/curl.h"),
+);
+
+
+pub fn request (
+    allocator: std.mem.Allocator,
+    post: []const u8,
+    url: []const u8,
+) !std.ArrayList(u8) {
+
+    // global curl init, or fail
+    if (curl.curl_global_init(curl.CURL_GLOBAL_ALL) != curl.CURLE_OK)
+        return error.CURLGlobalInitFailed;
+    defer curl.curl_global_cleanup();
+
+    // curl easy handle init, or fail
+    const handle = curl.curl_easy_init() orelse return error.CURLHandleInitFailed;
+    defer curl.curl_easy_cleanup(handle);
+
+    var response_buffer = std.ArrayList(u8).init(allocator);
+
+    // superfluous when using an arena allocator, but
+    // important if the allocator implementation changes
+    errdefer response_buffer.deinit();
+
+    // setup curl options
+    if (curl.curl_easy_setopt(handle, curl.CURLOPT_URL, url.ptr) != curl.CURLE_OK)
+        return error.CouldNotSetURL;
+
+    if (curl.curl_easy_setopt(handle, curl.CURLOPT_POSTFIELDS, post.ptr) != curl.CURLE_OK)
+        return error.CouldNotSetPost;
+
+    // set write function callbacks
+    if (curl.curl_easy_setopt(
+        handle,
+        curl.CURLOPT_WRITEFUNCTION,
+        writeToArrayListCallback,
+    ) != curl.CURLE_OK)
+        return error.CouldNotSetWriteCallback;
+
+    if (curl.curl_easy_setopt(
+        handle,
+        curl.CURLOPT_WRITEDATA,
+        &response_buffer,
+    ) != curl.CURLE_OK)
+        return error.CouldNotSetWriteCallback;
+
+    // perform
+    if (curl.curl_easy_perform(handle) != curl.CURLE_OK)
+        return error.FailedToPerformRequest;
+
+    return response_buffer;
+}
+
+// From https://ziglang.org/learn/samples/#using-curl-from-zig
+fn writeToArrayListCallback(
+    data: *anyopaque,
+    size: c_uint,
+    nmemb: c_uint,
+    user_data: *anyopaque,
+) callconv(.C) c_uint {
+    var buffer = @intToPtr(*std.ArrayList(u8), @ptrToInt(user_data));
+    var typed_data = @intToPtr([*]u8, @ptrToInt(data));
+    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
+    return nmemb * size;
+}
+
