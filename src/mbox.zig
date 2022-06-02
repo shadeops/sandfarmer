9f96339ea53d7d0799733e031477c338a071be3a
jim price <shadeops@gmail.com>
2022-06-01 19:35:59 -0700

    Add a MsgBuffer storage type

--- src/mbox.zig
+++ src/mbox.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const MessageCounts = struct {
     const Self = @This();
     active: u32 = 0,
@@ -37,3 +39,79 @@ pub const MessageCounts = struct {
         return (self.active | self.blocked | self.done | self.err) != 0;
     }
 };
+
+pub const MsgType = enum(u3) {
+    active,
+    done,
+    blocked,
+    err,
+};
+
+pub const Msg = struct {
+    jid: u32,
+    owner: u16,
+    msg: MsgType, //u3
+};
+
+pub const MsgBuffer = struct {
+    const Self = @This();
+    msgs: []Msg,
+    pos: usize = 0,
+    size: usize = 0,
+
+    pub fn append(self: *Self, msg: Msg) !usize {
+        var new_size = self.size + 1;
+        if (new_size > self.msgs.len)
+            return error.FullMsgBuffer;
+        var append_pos = (self.pos + self.size) % self.msgs.len;
+        self.msgs[append_pos] = msg;
+        self.size = new_size;
+        return self.size;
+    }
+
+    pub fn next(self: *Self) ?Msg {
+        if (self.size == 0) return null;
+        var cur_pos = self.pos;
+        self.pos = (self.pos + 1) % self.msgs.len;
+        self.size = self.size - 1;
+        return self.msgs[cur_pos];
+    }
+};
+
+test "msg buffer" {
+    var msgs = [_]Msg{undefined} ** 3;
+    var buffer = MsgBuffer{
+        .msgs = msgs[0..],
+    };
+    try std.testing.expectEqual(3, msgs.len);
+
+    _ = try buffer.append(.{ .jid = 1, .msg = .active, .owner = 1 });
+    try std.testing.expectEqual(@as(usize, 1), buffer.size);
+    try std.testing.expectEqual(@as(usize, 0), buffer.pos);
+
+    _ = try buffer.append(.{ .jid = 2, .msg = .active, .owner = 1 });
+    try std.testing.expectEqual(@as(usize, 2), buffer.size);
+
+    _ = try buffer.append(.{ .jid = 3, .msg = .active, .owner = 1 });
+    try std.testing.expectEqual(@as(usize, 3), buffer.size);
+
+    try std.testing.expectError(
+        error.FullMsgBuffer,
+        buffer.append(.{ .jid = 4, .msg = .active, .owner = 1 }),
+    );
+
+    var m = buffer.next();
+    try std.testing.expectEqual(@as(usize, 1), buffer.pos);
+    try std.testing.expectEqual(@as(usize, 2), buffer.size);
+    try std.testing.expectEqual(@as(u32, 1), m.?.jid);
+
+    _ = try buffer.append(.{ .jid = 4, .msg = .active, .owner = 1 });
+    try std.testing.expectEqual(@as(usize, 3), buffer.size);
+    try std.testing.expectEqual(@as(usize, 1), buffer.pos);
+
+    try std.testing.expectEqual(@as(u32, 4), buffer.msgs[0].jid);
+    m = buffer.next();
+    m = buffer.next();
+    m = buffer.next();
+    try std.testing.expect(buffer.next() == null);
+}
